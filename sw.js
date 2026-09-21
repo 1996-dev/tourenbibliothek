3d181dfb1558fa401ba1816f59f0a9c4aa1b13c4
1996-dev <andre.kloeck.1996@gmail.com>
2026-09-21 18:43:50 +0200

    Vilsalpsee und Nagelfluhkette West, Wertacher Hoernle abgehakt

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
 
    VERSION setzt build.py aus dem Inhalt der Seite - neuer Inhalt, neuer Cache. */
 
-const VERSION = "tourenbib-2026-09-21-e2334d3a61";
+const VERSION = "tourenbib-2026-09-21-99b4b1ba73";
 const SCHRIFT_CACHE = "tourenbib-schriften-v1";
 
 const SCHALE = [
